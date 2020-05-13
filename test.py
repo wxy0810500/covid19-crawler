e83dd6963d30a8bed9c81e11de5e48e5a3ff3451
wxy0810500 <newman_wang@sina.com>
2020-05-13 20:13:36 +0800

    remove unused files

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from utils.proxy_ip_pool import getProxyURL
-
-
-proxies = {"https": "218.241.219.84:3128"}
-s = requests.Session()
-s.proxies = proxies
-r = s.get("https://ip.chinaz.com/")
-print(r.text)
